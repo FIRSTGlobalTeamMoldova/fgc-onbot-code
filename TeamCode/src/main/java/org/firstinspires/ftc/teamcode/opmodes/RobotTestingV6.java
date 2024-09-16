03475f64d7f4893d6906ab9a92e2f27ab00a315a
stolidus-cpu <113309230+stolidus-cpu@users.noreply.github.com>
2024-09-16 19:21:00 +0300

    Hdrive imu (#6)
    * Proportional gyro control
    
    * Added FTC dashboard
    
    * Fix linear motion
    
    ---------
    
    Co-authored-by: Mihai <mihai.marandici2008@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RobotTestingV6.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.components.BaseVelocityTelemetry;
+import org.firstinspires.ftc.teamcode.components.BasketComponentV1;
+import org.firstinspires.ftc.teamcode.components.FlapsComponentV1;
+import org.firstinspires.ftc.teamcode.components.HDriveComponentV3;
+import org.firstinspires.ftc.teamcode.components.JoystickTelemetry;
+import org.firstinspires.ftc.teamcode.components.LinearMotionComponentV2;
+import org.firstinspires.ftc.teamcode.utilities.ComponentOpMode;
+
+@TeleOp(name = "Experimental Gyro")
+public class RobotTestingV6 extends ComponentOpMode {
+    @Override
+    public void componentsSetup() {
+        components.add(new HDriveComponentV3());
+        components.add(new LinearMotionComponentV2());
+        components.add(new BasketComponentV1());
+        components.add(new FlapsComponentV1());
+
+        components.add(new BaseVelocityTelemetry());
+        components.add(new JoystickTelemetry());
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/RobotConstants.java
@@ -9,14 +9,12 @@ public class RobotConstants {
     public static final String HDRIVE_RIGHT = "h drive right";
     public static final String LINEAR_LEFT = "linear left";
     public static final String LINEAR_RIGHT = "linear right";
-    public static final String BASKET_LEFT = "basket left";
-    public static final String BASKET_RIGHT = "basket right";
     public static final String BASKET_RIGHT_SERVO = "basket right servo";
     public static final String BASKET_LEFT_SERVO = "basket left servo";
     public static final String FLAPS_LEFT = "flaps left";
     public static final String FLAPS_RIGHT = "flaps right";
 
     public static final int DRIVE_CPR = 28 * 15;
-    public static final int HDRIVE_CPR = 28 * 9;
+    public static final int HDRIVE_CPR = 28 * 12;
     public static final int HDHEX_RPM = 6000;
 }
