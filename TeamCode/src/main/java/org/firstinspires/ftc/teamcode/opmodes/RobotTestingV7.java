cb13b4bcc313d083273b76f92f0beeb17b2a9859
Mihai <mihai.marandici2008@gmail.com>
2024-09-18 17:50:41 +0300

    Holonomic gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RobotTestingV7.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RobotTestingV7.java
@@ -6,15 +6,16 @@ import org.firstinspires.ftc.teamcode.components.BaseVelocityTelemetry;
 import org.firstinspires.ftc.teamcode.components.BasketComponentV1;
 import org.firstinspires.ftc.teamcode.components.FlapsComponentV1;
 import org.firstinspires.ftc.teamcode.components.HDriveComponentV3;
+import org.firstinspires.ftc.teamcode.components.HDriveComponentV4;
 import org.firstinspires.ftc.teamcode.components.JoystickTelemetry;
 import org.firstinspires.ftc.teamcode.components.LinearMotionComponentV2;
 import org.firstinspires.ftc.teamcode.utilities.ComponentOpMode;
 
-@TeleOp(name = "Old")
-public class RobotTestingV6 extends ComponentOpMode {
+@TeleOp(name = "Main")
+public class RobotTestingV7 extends ComponentOpMode {
     @Override
     public void componentsSetup() {
-        components.add(new HDriveComponentV3());
+        components.add(new HDriveComponentV4());
         components.add(new LinearMotionComponentV2());
         components.add(new BasketComponentV1());
         components.add(new FlapsComponentV1());
